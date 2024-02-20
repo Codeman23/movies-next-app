361caf37bc3216beae877c4c9e804ec438b2bcba
Anton Dubovskiy <a.n.dibovskiy@gmail.com>
2024-02-20 12:38:58 +0300

    new project added

---
+++ app/components/screens/home/Home.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+import Layout from '@/components/layout/Layout';
+
+import { IHome } from './home.interface';
+
+const Home: FC<IHome> = () => {
+	return <Layout>Home</Layout>;
+};
+
+export default Home;
