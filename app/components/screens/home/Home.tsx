e4cc32d67a9ed1b80ba64dff50572cd067f15645
Anton Dubovskiy <a.n.dibovskiy@gmail.com>
2024-02-21 15:25:00 +0300

    meta and skeleton added

--- app/components/screens/home/Home.tsx
+++ app/components/screens/home/Home.tsx
@@ -1,11 +1,23 @@
 import { FC } from 'react';
 
-import Layout from '@/components/layout/Layout';
+import Heading from '@/components/ui/heading/Heading';
+
+import Meta from '@/utils/meta/Meta';
 
 import { IHome } from './home.interface';
 
 const Home: FC<IHome> = () => {
-	return <Layout>Home</Layout>;
+	return (
+		<Meta
+			title="Watch movies online"
+			description="Watch movies online and TV everyday."
+		>
+			<Heading
+				title="Watch movies online"
+				className="text-gray-500 mb-8 text-xl"
+			/>
+		</Meta>
+	);
 };
 
 export default Home;
