38b7ecd5884efa00f8bb492366673352ec43b66c
Anton Dubovskiy <a.n.dibovskiy@gmail.com>
2024-02-21 14:16:15 +0300

    main page menu updates

--- next.config.mjs
+++ next.config.mjs
@@ -10,10 +10,13 @@ const nextConfig = {
 	},
 	async rewrites() {
 		return [
-			{ source: '/api/:path*', destination: 'http://localhost:4200/api/path*' },
+			{
+				source: '/api/:path*',
+				destination: `http://localhost:4200/api/:path*`,
+			},
 			{
 				source: '/uploads/:path*',
-				destination: 'http://localhost:4200/uploads/path*',
+				destination: `http://localhost:4200/uploads/:path*`,
 			},
 		];
 	},
