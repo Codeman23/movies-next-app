76e7fb28a39da8b6f9863fa7d288a9b5d8c6ef58
Anton Dubovskiy <a.n.dibovskiy@gmail.com>
2024-06-23 12:44:01 +0300

    interceptors added

---
+++ pages/manage/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next';
+
+// import Home from '@/components/screens/home/Home';
+
+const AdminPage: NextPage = () => {
+	// return <Home />;
+	return <></>;
+};
+
+export default AdminPage;
---
+++ pages/profile.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next';
+
+// import Home from '@/components/screens/home/Home';
+
+const ProfilePage: NextPage = () => {
+	// return <Home />;
+	return <></>;
+};
+
+export default ProfilePage;
