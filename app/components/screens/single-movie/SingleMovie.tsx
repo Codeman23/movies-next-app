9f9159ee40ab8bae8726f9f6af995e8fb31e27d1
Anton Dubovskiy <a.n.dibovskiy@gmail.com>
2024-08-23 15:59:38 +0300

    video player added

--- app/components/screens/single-movie/SingleMovie.tsx
+++ app/components/screens/single-movie/SingleMovie.tsx
@@ -1,10 +1,16 @@
 import { FC } from 'react';
+import dynamic from 'next/dynamic';
 import Meta from '@/utils/meta/Meta';
 import Banner from '@/components/ui/banner/Banner';
 import SubHeading from '@/components/ui/heading/SubHeading';
 import Gallery from '@/components/ui/gallery/Gallery';
 import { IMoviePageProps } from '../../../../pages/movie/[slug]';
 import Content from './Content/Content';
+
+const DynamicPlayer = dynamic(
+	() => import('@/components/ui/video-player/VideoPlayer'),
+	{ ssr: false }
+);
 
 const SingleMovie: FC<IMoviePageProps> = ({ similarMovies, movie }) => {
 	return (
@@ -16,6 +22,10 @@ const SingleMovie: FC<IMoviePageProps> = ({ similarMovies, movie }) => {
 				/>
 			)}
 			{/* Video player */}
+			<DynamicPlayer
+				slug={`${movie?.slug}`}
+				videoSource={`${movie?.videoUrl}`}
+			/>
 
 			<div className="mt-12">
 				<SubHeading title="Similar" />
